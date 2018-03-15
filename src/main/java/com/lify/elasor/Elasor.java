337639260fdde7d2847434e9378e4a3fe56d7c49
elasor <elasor@163.com>
2018-03-15 10:25:37 +0800

    update 2 also
    update 2 also

--- src/main/java/com/lify/elasor/Elasor.java
+++ src/main/java/com/lify/elasor/Elasor.java
@@ -1,16 +1,25 @@
 package com.lify.elasor;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import com.lify.elasor.http.ElasorHttp;
 import com.lify.elasor.message.ElasorLog;
+
+import java.lang.reflect.Method;
 
 /**
  * @author Elasor
  */
 @SuppressWarnings("unused")
 public class Elasor {
+
+    private Elasor(){}
 
     public static void init(Context context, boolean isReleaseLog){
         ElasorLog.init(context, isReleaseLog);
@@ -20,5 +29,83 @@ public class Elasor {
     public static boolean isDebugMode(Context context){
         ApplicationInfo ai = context.getApplicationContext().getApplicationInfo();
         return null != ai && (ai.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+    }
+
+
+    /**
+     * 返回当前程序版本名
+     */
+    public static String getAppVersionName(Context context) {
+        String versionName = "0.0.0";
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            versionName = pi.versionName;
+            if (versionName == null || versionName.length() <= 0) {
+                return "0.0.0";
+            }
+        } catch (Exception e) {
+            Log.e("VersionInfo", "Exception", e);
+        }
+        return versionName;
+    }
+
+    /**
+     * 获取IMEI
+     * 需要READ_PHONE_STATE权限 {@link android.Manifest.permission#READ_PHONE_STATE}
+	 */
+    @SuppressLint("HardwareIds")
+    public static String getDeviceIMEI(Context context) {
+        String deviceIMEI = "";
+
+        try {
+            TelephonyManager tm = (TelephonyManager)
+                    context.getSystemService(Context.TELEPHONY_SERVICE);
+            deviceIMEI = tm.getDeviceId();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return deviceIMEI;
+    }
+
+    /**
+     * 获取序列号
+     * @return 序列号
+     */
+    public static String getSerialNumber() {
+        String serial = null;
+        try {
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Method get = c.getMethod("get", String.class);
+            serial = (String) get.invoke(c, "ro.serialno");
+            System.out.println(serial);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return serial;
+    }
+
+
+    /**
+     * dp转换成px
+     * @param context 上下文
+     * @param dp dp
+     * @return px
+     */
+    public static int dp2px(Context context, float dp){
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
+    /**
+     * px转换成dp
+     * @param context 上下文
+     * @param px px
+     * @return dp
+     */
+    public static int px2dp(Context context, float px){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (px / scale + 0.5f);
     }
 }
--- src/main/java/com/lify/elasor/secret/ElasorSecret.java
+++ src/main/java/com/lify/elasor/secret/ElasorSecret.java
@@ -1,5 +1,82 @@
 package com.lify.elasor.secret;
 
+import android.util.Base64;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @author Elasor
+ */
+@SuppressWarnings("unused")
 public class ElasorSecret {
+
+    public static String md5(String encodeStr) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(encodeStr.getBytes(Charset.forName("UTF-8")));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("there is no such algorithm which named MD5!", e);
+        }
+
+        StringBuilder hex = new StringBuilder();
+        for (byte b : hash) {
+            String s = String.format("%02x", b);
+            hex.append(s);
+        }
+        return hex.toString();
+    }
+
+    //未测试
+    public static String encodeBase64(String msg) {
+        byte[] encode = Base64.encode(msg.getBytes(Charset.forName("utf-8")), Base64.NO_WRAP);
+        return new String(encode, Charset.forName("utf-8"));
+    }
+
+    //未测试
+    public static String decodeBase64(String secret) {
+        return new String(Base64.decode(secret, Base64.DEFAULT), Charset.forName("utf-8"));
+    }
+
+    //未测试
+    public static String encodeDES(String src, String keyStr) {
+        String result = null;
+        try {
+            //根据算法和秘钥字符串，创建秘钥
+            SecretKeySpec key = new SecretKeySpec(keyStr.getBytes(), "DES");
+            //根据算法创建密码对象
+            Cipher cipher = Cipher.getInstance("DES");
+            //指定为编码模式，指定秘钥
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            //执行得到密文的字节数组
+            byte[] bytes = cipher.doFinal(src.getBytes(Charset.forName("utf-8")));
+            //密码也采用64种基本字符，否则会乱码
+            result = Base64.encodeToString(bytes, Base64.DEFAULT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    //未测试
+    public static String decodeDES(String secret, String keyStr) {
+        String result = null;
+        try {
+            SecretKeySpec key = new SecretKeySpec(keyStr.getBytes(), "DES");
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] bytes = cipher.doFinal(Base64.decode(secret, Base64.DEFAULT));
+            result = new String(bytes, Charset.forName("utf-8"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 
 }
